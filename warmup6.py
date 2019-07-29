9b0e0d0fb6b2d86815189f006d7987490581e557
EC2 Default User <ec2-user@ip-172-31-47-16.us-east-2.compute.internal>
2019-07-29 18:58:11 +0000

    hi

---
+++ warmup6.py
@@ -0,0 +1,20 @@
+items_store= ["Candy", "Pasta", "Pizza", "Sushi", "Chicken"]
+
+# for items in items_store:
+#     print("items + "is" + $10")
+    
+items= input("What do you want to buy? We have candy, pasta, pizza, sushi, and chicken.")
+def items_store(items):
+    if items == candy:
+        print("Your purchase is $1.00")
+    elif mood == pasta:
+        print("Your purchase is $5.00")
+    elif mood == pizza:
+        print("Your purchase is $3.00")
+items_store("candy")
+
+# items_store = [
+#     {"Candy": "$1.00"},
+#     {"Pasta": "$5.00"},
+#     {""
+#     ]
